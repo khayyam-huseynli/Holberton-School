1cbd6aafcd03319c1cf17926a83172a0b90483ff
Khayyam Huseynli <53341322+khayyam-huseynli@users.noreply.github.com>
2023-09-09 02:56:02 +0400

    Add files via upload

---
+++ behavior.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+
+    thumbnailElement.addEventListener("click", function() {
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    thumbnailElement.className;
+            
+        if (thumbnailElement.className == "") {
+            thumbnailElement.className = "small";
+        } else{
+            thumbnailElement.className = "";
+        }
+      });
+
+});
+
